1908458901cc7b0542ff1a2ed386afe6c64a113c
Kaleshe Alleyne-Vassel <kalesheav@gmail.com>
2019-04-02 23:53:02 +0100

    Turned into a one-page website

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -3,7 +3,7 @@ var mobileNav = document.getElementById('mobile-nav');
 
 
 function menu(){hamburger.addEventListener('click', function(){
-  if(mobileNav.style.display === "none"){
+  if(mobileNav.style.display = "none"){
     mobileNav.style.display = "flex";
     hamburger.classList.add('close-icon');
   } else {
