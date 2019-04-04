a62fbf452c63cedd88690d880461f2c83432fad7
Kaleshe Alleyne-Vassel <kalesheav@gmail.com>
2019-04-04 21:20:05 +0100

    Made hamburger fixed when it's pressed

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -8,10 +8,12 @@ function menu(){hamburger.addEventListener('click', function(){
     mobileMenu.style.display = "flex";
     mobileMenu.classList.add('display-menu');
     hamburger.classList.add('close-icon');
+    hamburger.classList.add('fix');
   } else {
     mobileMenu.style.display = "none";
     mobileMenu.classList.remove('display-menu');
     hamburger.classList.remove('close-icon');
+    hamburger.classList.remove('fix');
   };
 });};
 
