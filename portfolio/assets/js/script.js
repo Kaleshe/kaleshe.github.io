ad8524ca21279b82affa43ae4fc72704d83a1bc3
Kaleshe Alleyne-Vassel <kalesheav@gmail.com>
2019-04-04 21:05:05 +0100

    Fixed nav

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -3,12 +3,16 @@ var mobileMenu = document.getElementById('mobile-menu');
 
 
 function menu(){hamburger.addEventListener('click', function(){
-  if(mobileMenu.style.display === "none"){
+
+  if(!mobileMenu.classList.contains('display-menu')){
     mobileMenu.style.display = "flex";
+    mobileMenu.classList.add('display-menu');
     hamburger.classList.add('close-icon');
   } else {
     mobileMenu.style.display = "none";
+    mobileMenu.classList.remove('display-menu');
     hamburger.classList.remove('close-icon');
+    console.log('second');
   };
 });};
 
