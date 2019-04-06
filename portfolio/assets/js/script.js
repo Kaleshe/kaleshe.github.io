e7ca99cf624c6f8efd9a3fb1222691974be51e94
Kaleshe Alleyne-Vassel <kalesheav@gmail.com>
2019-04-06 10:45:04 +0100

    Changed p and h2 to rgba colours

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -1,20 +1,17 @@
 var hamburger = document.getElementById('hamburger');
 var mobileMenu = document.getElementById('mobile-menu');
 
-
-function menu(){hamburger.addEventListener('click', function(){
+function menuToggle(){hamburger.addEventListener('click', function(){
 
   if(!mobileMenu.classList.contains('display-menu')){
     mobileMenu.style.display = "flex";
     mobileMenu.classList.add('display-menu');
     hamburger.classList.add('close-icon');
-    hamburger.classList.add('fix');
   } else {
     mobileMenu.style.display = "none";
     mobileMenu.classList.remove('display-menu');
     hamburger.classList.remove('close-icon');
-    hamburger.classList.remove('fix');
   };
 });};
 
-menu();
+menuToggle();
